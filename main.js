00e8c007a25caf12de8a1b208305a445b15ce2e0
Luiz Nai <luiz.neosnk@gmail.com>
2021-02-10 19:41:29 -0300

    Cleaning code.

--- main.js
+++ main.js
@@ -8,49 +8,48 @@ function pick_color(myColor, vmuValue) {
 
 function change_color(element) {
     console.log(element);
-    document.getElementById(element).classList.remove("color1","color2");
+    document.getElementById(element).classList.remove("black","white");
     document.getElementById(element).classList.add(currentColor);
     document.getElementById(element).setAttribute("vmuvalue", currentVMUValue)
 }
 
 function createScreen() {
-    
-    htmlGenerator = "";
+    htmlGenerator = '';
+    count = 0;
+    for(i=0;i<512;i++) {
+        if(count==0) {
+            htmlGenerator +=`<div class="row">`;
+        }
+        
+        htmlGenerator += `<div id="`+i+`" class="black border field" vmuvalue="0x00" onClick="change_color('`+i+`')"></div>`
 
-    for(i=1;i<65;i++) {
-        htmlGenerator +=`<div class="row">
-                        <div class="col-2 text-center">`+ i +`</div>`;
-        for(j=1;j<9;j++){
-            htmlGenerator += `
-            <div id="`+i+`-`+j+`" class="col-1 color1 text-center" vmuvalue="0x00" onClick="change_color('`+i+`-`+j+`')">
-                <small></small>
-            </div>`
+        count++;
+        if(count==16) {
+            count=0;
         }
 
-        htmlGenerator +=`
-            <div class="col-2 text-center"></div>
-        </div>`;
+        if(count==0) {
+            htmlGenerator +=`</div>`;
+        }
     }
 
     document.getElementById('screen').innerHTML = htmlGenerator;
 }
+
+createScreen();
 
 function generateArray() {
     document.getElementById("resultText").value = "";
     finalObject = "";
     finalObject += "{";
     finalObject+=`0x00,0xf0,0x0d,0xf0,0x0f,0xf0,0x00,0x0f,0x44,0xf4,0x62,0xfb,0x96,0xff,0xca,0xff,0xff,0xff,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,`;
-    for(i=1;i<65;i++) {
-        for(j=1;j<9;j++){
-            finalObject+=document.getElementById(i+"-"+j).getAttribute("vmuvalue");
+    for(i=0;i<512;i++) {
+        finalObject+=document.getElementById(i).getAttribute("vmuvalue");
 
-            if(i+"-"+j !== '64-8') {
-                finalObject+=",";
-            }
+        if(i !== 511) {
+            finalObject+=",";
         }
     }
     finalObject += "};";
     document.getElementById("resultText").value = finalObject;
 }
-
-createScreen();
