3ab0ead27b739332898e95d55b1246290b046819
Luiz Nai <luiz.neosnk@gmail.com>
2021-02-10 23:23:34 -0300

    Adding colors and creating function to load from hex.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function pick_color(myColor, vmuValue) {
 
 function change_color(element) {
     console.log(element);
-    document.getElementById(element).classList.remove("black","white");
+    document.getElementById(element).classList.remove("color1","color2", "color3", "color4", "color5", "color6", "color7");
     document.getElementById(element).classList.add(currentColor);
     document.getElementById(element).setAttribute("vmuvalue", currentVMUValue)
 }
@@ -21,7 +21,7 @@ function createScreen() {
             htmlGenerator +=`<div class="row">`;
         }
         
-        htmlGenerator += `<div id="`+i+`" class="black border field" vmuvalue="0x00" onClick="change_color('`+i+`')"></div>`
+        htmlGenerator += `<div id="`+i+`" class="color1 border field" vmuvalue="0x00" onClick="change_color('`+i+`')"></div>`
 
         count++;
         if(count==16) {
@@ -42,7 +42,7 @@ function generateArray() {
     document.getElementById("resultText").value = "";
     finalObject = "";
     finalObject += "{";
-    finalObject+=`0x00,0xf0,0x0d,0xf0,0x0f,0xf0,0x00,0x0f,0x44,0xf4,0x62,0xfb,0x96,0xff,0xca,0xff,0xff,0xff,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,0x00,0xf0,`;
+    finalObject+=`0x00,0xF0,0x0F,0x0F,0x69,0xF6,0xCC,0xFC,0x88,0xF3,0xFC,0xFF,0xFC,0xFF,0x99,0xF9,0x77,0xF7,0x44,0xF4,0x21,0xF2,0xA0,0xFF,0x61,0xF9,0x93,0xFC,0xCB,0xFC,0x00,0xF0,`;
     for(i=0;i<512;i++) {
         finalObject+=document.getElementById(i).getAttribute("vmuvalue");
 
@@ -52,4 +52,49 @@ function generateArray() {
     }
     finalObject += "};";
     document.getElementById("resultText").value = finalObject;
+}
+
+function loadArray() {
+    var myObject = document.getElementById("resultText").value;
+    myObject = myObject.replace("{", "");
+    myObject = myObject.replace("};", "");
+    var arr = myObject.split(','); 
+    
+    for(i=32;i<arr.length;i++) {
+        document.getElementById(i-32).classList.remove("color1","color2", "color3", "color4", "color5", "color6", "color7");
+
+        switch(arr[i]) {
+            case "0x00":
+                document.getElementById(i-32).classList.add("color1");
+            break;
+
+            case "0x44":
+                document.getElementById(i-32).classList.add("color2");
+            break;
+
+            case "0x33":
+                document.getElementById(i-32).classList.add("color3");
+            break;
+
+            case "0x66":
+                document.getElementById(i-32).classList.add("color4");
+            break;
+
+            case "0x22":
+                document.getElementById(i-32).classList.add("color5");
+            break;
+
+            case "0xCC":
+                document.getElementById(i-32).classList.add("color6");
+            break;
+
+            case "0x77":
+                document.getElementById(i-32).classList.add("color7");
+            break;
+
+            default:
+                document.getElementById(i-32).classList.add("color1");
+            break;
+        }
+    }
 }
